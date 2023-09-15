db0f4aa4a26ec99deab751ccfeb84c975df17c9e
isaralamit <145096143+isaralamit@users.noreply.github.com>
2023-09-14 23:10:12 -0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def checkleap(year):
- if ((year % 400== 0)or (year % 100 != 0)and (year  % 4 == 0)):
-   print('leapyear')
- else:
-     print( 'not leap')
-year = int(input('enter year:'))
-checkleap(year)
-     
